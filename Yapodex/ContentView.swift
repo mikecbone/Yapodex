75b4d23bab0bc6e405a0d7be2df9a746311819ae
Mike Bone <mikecbone@gmail.com>
2021-04-20 21:30:41 +0100

    Initial Commit

---
+++ Yapodex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yapodex
+//
+//  Created by Mike Bone on 20/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yapodex/YapodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapodexApp.swift
+//  Yapodex
+//
+//  Created by Mike Bone on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct YapodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
